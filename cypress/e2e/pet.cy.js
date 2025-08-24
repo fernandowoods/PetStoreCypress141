ba5b092a7c8d83c5208e6a8531abe8ea261830b2
fernandowoods <fewoods16@gmail.com>
2025-08-23 23:43:13 -0300

    CRUD  + POST Data Driven

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -88,24 +88,4 @@ describe('CRUD da entidade Pet', () => {
       })
     })
   })
-  // GET Pet Data Driven
-  listaPet.forEach((element) => {
-    it(`GET Pet Data Driven - ${element.name}`, () => {
-      cy.request({
-        method: 'GET',
-        url: `/pet/${.id}`
-      }).then(({ status, body }) => {
-        expect(status).to.eq(200)
-        expect(body.id).to.eq(element.id)
-        expect(body.name).to.eq(element.name)
-        expect(body.category.id).to.eq(element.category.id)
-        expect(body.category.name).to.eq(element.category.name)
-        expect(body.tags[0].id).to.eq(element.tags[0].id)
-        expect(body.tags[0].name).to.eq(element.tags[0].name)
-        expect(body.status).to.eq(element.status)
-      })
-    })
-  })
-
-
 })
